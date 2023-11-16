5624db2619e438fa75b535b654e8671297eed221
chathuraprabashwara <chathuraprabashwara@gmail.com>
2023-11-16 11:40:03 +0530

    add image with questions

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,36 +1,80 @@
 import React, { useState } from 'react';
 import './Questions.css'; // Import the CSS file
+import Grid from '@mui/material/Grid';
 
 function Questions({ data }) {
-    const [colorId,setColorId] = useState()
-    const [answers, setAnswer] = useState([])
-    const handleAnswer = (item,id) => {
-        if(answers.length >0){
-           const checkAvailability =  answers.filter((val) => +val[0] !==item)
-           setAnswer([...checkAvailability,`${item}${id}`])
-        }
-        else{
-            setAnswer((prev) => [...prev,`${item}${id}`])
-        }
-            console.log(item)
-            console.log(id)
-    }
-  return (
-    <>
-      {data.map((item, index) => (
+	const [colorId, setColorId] = useState();
+	const [answers, setAnswer] = useState([]);
+	const handleAnswer = (item, id) => {
+		if (answers.length > 0) {
+			const checkAvailability = answers.filter((val) => +val[0] !== item);
+			setAnswer([...checkAvailability, `${item}${id}`]);
+		} else {
+			setAnswer((prev) => [...prev, `${item}${id}`]);
+		}
+		console.log(item);
+		console.log(id);
+	};
+	return (
+		<>
+			{data.map((item, index) => (
+                !item.image?
+	(			
         <div key={index} className="questionContainer2">
-          <p className="questionText">{index +1}) {item.question}</p>
-          <div className="answerContainer">
-            {item.answers.map((val, idx) => (
-              <div key={idx} className="answerText" style={{backgroundColor:answers.includes(`${item.id}${idx}`) && '#DA0E3F' }} onClick={()=>handleAnswer(item.id,idx)}>
-               {idx +1}) {val}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </>
-  );
+				  <p className="questionText">{index +1}) {item.question}</p>
+				  <div className="answerContainer">
+				    {item.answers.map((val, idx) => (
+				      <div key={idx} className="answerText" style={{backgroundColor:answers.includes(`${item.id}${idx}`) && '#DA0E3F' }} onClick={()=>handleAnswer(item.id,idx)}>
+				       {idx +1}) {val}
+				      </div>
+				    ))}
+				  </div>
+				</div>
+                )
+				:(<div
+					key={index}
+					className="questionContainer2"
+				>
+					<Grid
+						container
+						sx={{ width: '100%' }}
+					>
+						<Grid xs="10">
+							<p className="questionText">
+								{index + 1}) {item.question}
+							</p>
+							<div className="answerContainer">
+								{item.answers.map((val, idx) => (
+									<div
+										key={idx}
+										className="answerText"
+										style={{
+											backgroundColor:
+												answers.includes(`${item.id}${idx}`) && '#DA0E3F',
+										}}
+										onClick={() => handleAnswer(item.id, idx)}
+									>
+										{idx + 1}) {val}
+									</div>
+								))}
+							</div>
+						</Grid>
+						<Grid xs="2">
+							<img
+								style={{
+									width: '193px',
+									height: '166px',
+									marginTop: '10px',
+								}}
+								src={item.url}
+								alt="question"
+							/>
+						</Grid>
+					</Grid>
+				</div>)
+			))}
+		</>
+	);
 }
 
 export default Questions;
--- src/pages/ModulePage.js
+++ src/pages/ModulePage.js
@@ -6,18 +6,131 @@ import { IconButton } from '@mui/material';
 
 function ModulePage() {
 	const [open2, setOpen2] = useState(true);
+  const [qCount,setQCount] = useState(0); 
+  
   const data=[{
     id:1,
-    question:"what is the name of ...",
-    answers:["answer1","answer2","answer3" ,"answer4"]
+    question:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has...",
+    answers:["answer1","answer2","answer3" ,"answer4"],
+    image:true,
+    url:'https://flyingmag.sfo3.digitaloceanspaces.com/flyingma/wp-content/uploads/2022/06/23090933/AdobeStock_249454423-scaled.jpeg'
   },
   {
     id:2,
     question:"what is the name of ...",
     answers:["answer1","answer2","answer3" ,"answer4"]
-  }
+  },
+  {
+    id:3,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:4,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:5,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:6,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:7,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:8,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:9,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:10,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:11,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:12,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:13,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:14,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:15,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:16,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:17,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:18,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:19,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:20,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:21,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:22,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:23,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
+  {
+    id:24,
+    question:"what is the name of ...",
+    answers:["answer1","answer2","answer3" ,"answer4"]
+  },
 ]
-// [12,24,]
 
 	return (
 		<>
@@ -41,7 +154,7 @@ function ModulePage() {
 						alignItems: 'center',
 					}}
 				>
-					<p style={{ color: '#BBE1FA' }}>Total Questions - 10</p>
+					<p style={{ color: '#BBE1FA' }}>Total Questions - {qCount}</p>
 					<IconButton
 						style={{ marginLeft: '10px', width: '45px', height: '45px' }}
 					>
@@ -58,11 +171,12 @@ function ModulePage() {
 
 				}}
 			>
-				<Questions data={data} />
+				<Questions data={qCount !==0 ?data.slice(0,qCount) : []} />
 			</div>
 			<Modal
 				open2={open2}
 				setOpen2={setOpen2}
+        setQCount={setQCount}
 			/>
 		</>
 	);
