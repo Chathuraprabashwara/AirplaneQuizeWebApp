b50ec3456fd787c9775e76b6f4bc382f68cc2bf4
chathuraprabashwara <chathuraprabashwara@gmail.com>
2023-11-24 01:42:12 +0530

    add subcategory data

--- src/Components/Module.js
+++ src/Components/Module.js
@@ -8,14 +8,50 @@ const subCategory = [
 		id: 1,
 		name: 'module name 1',
 	},
-  {
+	{
 		id: 2,
 		name: 'module name 2',
 	},
-  {
+	{
 		id: 3,
 		name: 'module name 4',
-	}
+	},
+	{
+		id: 3,
+		name: 'module name 1',
+	},
+	{
+		id: 5,
+		name: 'module name 2',
+	},
+	{
+		id: 6,
+		name: 'module name 4',
+	},
+	{
+		id: 7,
+		name: 'module name 1',
+	},
+	{
+		id: 8,
+		name: 'module name 2',
+	},
+	{
+		id: 9,
+		name: 'module name 4',
+	},
+	{
+		id: 10,
+		name: 'module name 1',
+	},
+	{
+		id: 11,
+		name: 'module name 2',
+	},
+	{
+		id: 12,
+		name: 'module name 4',
+	},
 ];
 
 function Module({ name }) {
@@ -24,7 +60,9 @@ function Module({ name }) {
 		<div
 			className="ModuleCard"
 			onClick={() => {
-				navigate(`/subCategory/3`, { state: { subCategory: subCategory, prop2: 'value2' } });
+				navigate(`/subCategory/3`, {
+					state: { subCategory: subCategory, prop2: 'value2' },
+				});
 			}}
 		>
 			{name}
