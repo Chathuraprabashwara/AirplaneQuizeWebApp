ed21c016c4a2df528c2eec0d7f964af5afa36b18
yashith <yashith98@gmail.com>
2023-12-05 21:45:14 +0530

    shuffling questions

--- src/pages/SubCategory.js
+++ src/pages/SubCategory.js
@@ -51,7 +51,7 @@ export default function SubCategory() {
 				)
 				.then((res) => {
 					navigate(`/module/${selectedCategory.toString()}`, {
-						state: { data: res.data, id: id },
+						state: { data: shuffle(res.data), id: id },
 					});
 				});
 		} else {
@@ -144,4 +144,21 @@ export default function SubCategory() {
 			</div>
 		</div>
 	);
+}
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
